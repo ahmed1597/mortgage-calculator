76f0bcb2b7fa2ac67bd07df9b04b5188b24af91b
ahmed1597 <ahmed.mahmoud1597@gmail.com>
2023-10-08 02:51:14 +0400

    added models with all functions

---
+++ app/Models/ExtraRepaymentSchedule.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ExtraRepaymentSchedule extends Model
+{
+    protected $fillable = [
+        'month_number',
+        'starting_balance',
+        'monthly_payment',
+        'principal_component',
+        'interest_component',
+        'extra_repayment_made',
+        'ending_balance',
+        'remaining_loan_term',
+    ];
+
+    public function loan()
+    {
+        return $this->belongsTo(Loan::class);
+    }
+}
